c80efeb338549ebab5b55ae38f4090f8693dc1a3
AmriGIT <rahmatamri123@gmail.com>
2021-09-25 07:36:27 +0700

    intro to modern js

--- js.js
+++ js.js
@@ -17,3 +17,43 @@ function clickButton() {
         password.value = ""
     }
 }
+
+
+const fnRegister = () =>{
+    const FName = document.form1.FullName.value
+    const age = document.form1.Age.value
+    console.log(FName)
+    console.log(age)
+    let view = document.querySelector(".view")
+    view.innerHTML = "Full Nama : "+FName + "<br> "+"Usia : " + age
+}   
+
+const fnRegister2 = (FullName,iDate,iEmail, Age) =>{
+    FullName,
+    iDate,
+    iEmail,
+    Age
+    let view = document.querySelector(".view")
+    view.innerHTML = "Full Nama : "+FullName + "<br> " + " Tanggal Lahir : "+ iDate +"<br>" +" Email "+ iEmail
+}
+const fnRegister3 = (FullName,userName) =>({
+    FullName,
+    userName
+})
+
+const fnRegistred = () =>{
+    const user = document.form1.userName.value
+    const fullName = document.form1.FullName.value
+
+    var array = fnRegister3(fullName,user)
+
+      let isiNama = document.querySelector(".isiNama")
+      let isiUser = document.querySelector(".isiuser")
+      isiNama.innerHTML += array.FullName + "</br>"
+      isiUser.innerHTML += array.userName+ "</br>"
+      
+    
+}
+
+
+
