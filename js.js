393df724f047e3291a258d5f0f4ce19884804976
AmriGIT <rahmatamri123@gmail.com>
2021-09-25 23:41:23 +0700

    intro to modern js up

--- js.js
+++ js.js
@@ -36,24 +36,68 @@ const fnRegister2 = (FullName,iDate,iEmail, Age) =>{
     let view = document.querySelector(".view")
     view.innerHTML = "Full Nama : "+FullName + "<br> " + " Tanggal Lahir : "+ iDate +"<br>" +" Email "+ iEmail
 }
-const fnRegister3 = (FullName,userName) =>({
+var fnRegister3 = (FullName,userName) =>({
     FullName,
     userName
 })
 
-const fnRegistred = () =>{
+var  arrayData   = [];
+var addBtn = document.querySelector('.addBtn');
+function dataku(FullName, userName) {
+    this.FullName = FullName;
+    this.userName = userName;
+  }
+  document.addEventListener("click", e =>{
+      console.log(e);
+      if(e.target.getAttribute("proses"))
+      fnRegistred2()
+  })
+
+const fnRegistred2 = () =>{
     const user = document.form1.userName.value
     const fullName = document.form1.FullName.value
+    const jk = document.form1.gender.value
+    const agama = document.form1.agama.value
+    const password = document.form1.password.value
+    const passwordConf = document.form1.iPasswordConf.value
+    const alamat = document.form1.alamat.value
+    const hobi = document.querySelector("[name='hobbies']:checked")?.value
+    if (!password == passwordConf) {
+        alert("Isi Password tidak sama")
+    }else{
 
-    var array = fnRegister3(fullName,user)
+        arrayData.push({
+            user,
+            fullName,
+            jk,
+            agama,
+            hobi,
+            alamat
+        })
+        console.table(arrayData)
+        document.querySelector('table>tbody').innerHTML =""
+        for (var index = 0; index < arrayData.length; index++) {
+            const data = arrayData[index]
+            document.querySelector('table>tbody').innerHTML +=`
+            <tr>
+            <td>${index+1}</td>
+            <td>${data.user}</td>
+            <td>${data.fullName}</td>
+            <td>${data.jk}</td>
+            <td>${data.agama}</td>
+            <td>${data.hobi}</td>
+            <td>${data.alamat}</td>
+            
+            </tr>
+            `
+            console.log(data.hobi)
+        }
+    }
 
-      let isiNama = document.querySelector(".isiNama")
-      let isiUser = document.querySelector(".isiuser")
-      isiNama.innerHTML += array.FullName + "</br>"
-      isiUser.innerHTML += array.userName+ "</br>"
-      
     
 }
 
-
+const __init = () =>{
+    fnRegister2()
+}
 
