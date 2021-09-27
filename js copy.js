4cbaebd744e3e3dbc978db7165f64468f6b5327b
AmriGIT <rahmatamri123@gmail.com>
2021-09-27 13:21:58 +0700

    tugas array-method

---
+++ js copy.js
@@ -0,0 +1,128 @@
+
+function clickButton() {
+    let halamanUtama = document.querySelector(".halamanUtama")
+    let divLogin = document.querySelector(".login")
+    const username = document.fLogin.username
+    const password = document.fLogin.password
+    console.log(username.value);
+    console.log(password.value);
+    username.add
+    const dbuser = "AmriFathoni1"
+    const dbpass = "123456"
+    if (username.value == dbuser && password.value == dbpass) {
+        halamanUtama.classList.remove("hiden")
+        divLogin.classList.add("hiden")
+
+    } else {
+        alert("Username And Password Wrong!!")
+        username.value = ""
+        password.value = ""
+    }
+}
+
+
+document.addEventListener("click", e => {
+    console.log(e);
+    if (e.target.getAttribute("proses"))
+    fnRegistred2()
+})
+
+var arrayData = [];
+const PusharrayDatas = (user, fullName, jk, agama, hobi, alamat) => {
+    arrayData.push({
+        user,
+        fullName,
+        jk,
+        agama,
+        hobi,
+        alamat
+    })
+}
+var numbpages = Math.ceil(arrayData.length / records_per_page);
+const fnRegistred2 = (page) => {
+    const user = document.form1.userName.value
+    const fullName = document.form1.FullName.value
+    const jk = document.form1.gender.value
+    const agama = document.form1.agama.value
+    const password = document.form1.password.value
+    const passwordConf = document.form1.iPasswordConf.value
+    const alamat = document.form1.alamat.value
+    const hobi = document.querySelector("[name='hobbies']:checked")?.value
+    if (!password == passwordConf) {
+        alert("Isi Password tidak sama")
+    } else {
+        var btn_next = document.getElementById("btn_next");
+        var btn_prev = document.getElementById("btn_prev");
+        var listing_table = document.getElementById("listingTable");
+        var page_span = document.getElementById("page");
+
+        if (page < 1) page = 1;
+        if (page > numbpages) page = numbpages;
+
+        PusharrayDatas(user, fullName, jk, agama, hobi, alamat)
+        document.querySelector('table>tbody').innerHTML = ""
+        for (var i = (page - 1) * records_per_page; i < (page * records_per_page); i++) {
+            const data = arrayData[i]
+            document.querySelector('table>tbody').innerHTML += `
+            <tr>
+            <td>${i + 1}</td>
+            <td>${data.user}</td>
+            <td>${data.fullName}</td>
+            <td>${data.jk}</td>
+            <td>${data.agama}</td>
+            <td>${data.hobi}</td>
+            <td>${data.alamat}</td>
+
+            </tr>
+            `
+        }
+        page_span.innerHTML = page;
+
+        if (page == 1) {
+            btn_prev.style.visibility = "hidden";
+        } else {
+            btn_prev.style.visibility = "visible";
+        }
+
+        if (page == numPages()) {
+            btn_next.style.visibility = "hidden";
+        } else {
+            btn_next.style.visibility = "visible";
+        }
+
+    }
+
+
+}
+
+
+var current_page = 1;
+var records_per_page = 5;
+
+
+function prevPage() {
+    if (current_page > 1) {
+        current_page--;
+        fnRegistred2(current_page);
+    }
+}
+
+function nextPage() {
+    if (current_page < numPages()) {
+        current_page++;
+        fnRegistred2(current_page);
+    }
+}
+
+
+
+function numPages() {
+    return Math.ceil(arrayData.length / records_per_page);
+}
+
+
+
+const __init = () => {
+    fnRegistred2(1)
+}
+
--- js.js
+++ js.js
@@ -1,59 +1,25 @@
 
-function clickButton() {
-    let halamanUtama = document.querySelector(".halamanUtama")
-    let divLogin = document.querySelector(".login")
-    const username = document.fLogin.username
-    const password = document.fLogin.password
-    console.log(username.value);
-    console.log(password.value);
-    username.add
-    if (username.value == "AmriFathoni" && password.value == "123456") {
-        halamanUtama.classList.remove("hiden")
-        divLogin.classList.add("hiden")
-
-    } else {
-        alert("Username And Password Wrong!!")
-        username.value = ""
-        password.value = ""
-    }
+var list = []
+var pageList = [];
+var currentPage = 0;
+var numberPerPage = 10;
+var numberOfPages = 0;
+const PusharrayDatas = (user, fullName, jk, agama, hobi, alamat) => {
+    list.push({
+        user,
+        fullName,
+        jk,
+        agama,
+        hobi,
+        alamat
+    })
 }
-
-
-const fnRegister = () =>{
-    const FName = document.form1.FullName.value
-    const age = document.form1.Age.value
-    console.log(FName)
-    console.log(age)
-    let view = document.querySelector(".view")
-    view.innerHTML = "Full Nama : "+FName + "<br> "+"Usia : " + age
-}   
-
-const fnRegister2 = (FullName,iDate,iEmail, Age) =>{
-    FullName,
-    iDate,
-    iEmail,
-    Age
-    let view = document.querySelector(".view")
-    view.innerHTML = "Full Nama : "+FullName + "<br> " + " Tanggal Lahir : "+ iDate +"<br>" +" Email "+ iEmail
-}
-var fnRegister3 = (FullName,userName) =>({
-    FullName,
-    userName
+document.addEventListener("click", e => {
+    console.log(e);
+    if (e.target.getAttribute("proses"))
+    makeList()
 })
-
-var  arrayData   = [];
-var addBtn = document.querySelector('.addBtn');
-function dataku(FullName, userName) {
-    this.FullName = FullName;
-    this.userName = userName;
-  }
-  document.addEventListener("click", e =>{
-      console.log(e);
-      if(e.target.getAttribute("proses"))
-      fnRegistred2()
-  })
-
-const fnRegistred2 = () =>{
+function makeList() {
     const user = document.form1.userName.value
     const fullName = document.form1.FullName.value
     const jk = document.form1.gender.value
@@ -64,40 +30,81 @@ const fnRegistred2 = () =>{
     const hobi = document.querySelector("[name='hobbies']:checked")?.value
     if (!password == passwordConf) {
         alert("Isi Password tidak sama")
-    }else{
+    } else {
 
-        arrayData.push({
-            user,
-            fullName,
-            jk,
-            agama,
-            hobi,
-            alamat
-        })
-        console.table(arrayData)
-        document.querySelector('table>tbody').innerHTML =""
-        for (var index = 0; index < arrayData.length; index++) {
-            const data = arrayData[index]
-            document.querySelector('table>tbody').innerHTML +=`
+        PusharrayDatas(user, fullName, jk, agama, hobi, alamat)
+        document.querySelector('table>tbody').innerHTML = ""
+        for (x = 0; x < list.length; x++) {
+            const data = list[x]
+            document.querySelector('table>tbody').innerHTML += `
             <tr>
-            <td>${index+1}</td>
+            <td value='${x + 1}'>${x + 1}</td>
             <td>${data.user}</td>
             <td>${data.fullName}</td>
             <td>${data.jk}</td>
             <td>${data.agama}</td>
             <td>${data.hobi}</td>
             <td>${data.alamat}</td>
-            
+
             </tr>
             `
-            console.log(data.hobi)
+            numberOfPages = getNumberOfPages();
         }
+
     }
-
-    
 }
 
-const __init = () =>{
-    fnRegister2()
+function getNumberOfPages() {
+    return Math.ceil(list.length / numberPerPage);
 }
 
+function nextPage() {
+    currentPage += 1;
+    loadList();
+}
+
+function previousPage() {
+    currentPage -= 1;
+    loadList();
+}
+
+function firstPage() {
+    currentPage = 1;
+    loadList();
+}
+
+function lastPage() {
+    currentPage = numberOfPages;
+    loadList();
+}
+
+function loadList() {
+    var begin = ((currentPage - 1) * numberPerPage);
+    var end = begin + numberPerPage;
+
+    pageList = list.slice(begin, end);
+    drawList();
+    check();
+}
+
+function drawList() {
+    // const nomr = document.getElementsByTagName('td').value;
+    document.getElementById("list").innerHTML = "";
+    for (r = 0; r < pageList.length; r++) {
+        document.getElementById("list").innerHTML += Object.values(pageList[r]) + "<br/>";
+    }
+}
+
+function check() {
+    document.getElementById("next").disabled = currentPage == numberOfPages ? true : false;
+    document.getElementById("previous").disabled = currentPage == 1 ? true : false;
+    document.getElementById("first").disabled = currentPage == 1 ? true : false;
+    document.getElementById("last").disabled = currentPage == numberOfPages ? true : false;
+}
+
+function load() {
+    makeList();
+    loadList();
+}
+
+window.onload = load;
